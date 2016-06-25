d2068ddc44c29187ed0e1e996a007435c8a2e018
Stefan <palmerstefan@yahoo.com>
2016-06-25 11:48:36 -0600

    Changed formatting of lines
    [#120882805]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -1,10 +1,7 @@
 #!/usr/bin/env ruby
 
 def bottles(n)
-  puts "#{n} bottles of beer on the wall, #{n} bottles of beer."
-  puts ' '
-  puts "Take one down pass it around, #{n-1} bottles of beer on the wall."
-  puts ' '
+  puts "#{n} bottles of beer on the wall, #{n} bottles of beer.Take one down pass it around, #{n-1} bottles of beer on the wall."
 end
 
 99.downto(1){|n| bottles(n)}
