0c67b4365455a4cea16fccad0c1aa2751df87b3f
Stefan <palmerstefan@yahoo.com>
2016-06-27 20:31:18 -0600

    Fixed last lines by adding conditional
    [#120882805]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -1,7 +1,20 @@
 #!/usr/bin/env ruby
 
-def bottles(n)
-  puts "#{n} bottles of beer on the wall, #{n} bottles of beer.Take one down pass it around, #{n-1} bottles of beer on the wall."
+def bottles(number)
+  if number == 2
+    puts "#{number} bottles of beer on the wall, #{number} bottles of beer."
+    puts "Take one down and pass it around, #{number-1} bottle of beer on the wall."
+  elsif number == 1
+    puts "#{number} bottle of beer on the wall, #{number} bottle of beer."
+    puts 'Take one down and pass it around, no more bottles of beer on the wall.'
+  else
+    puts "#{number} bottles of beer on the wall, #{number} bottles of beer."
+    puts "Take one down and pass it around, #{number-1} bottles of beer on the wall."
+  end
 end
 
-99.downto(1){|n| bottles(n)}
+99.downto(1){|number| bottles(number)}
+
+puts 'No more bottles of beer on the wall, no more bottles of beer.'
+puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
+
